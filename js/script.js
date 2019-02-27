7506cd7d20c22080725ded6bf3d882e264f701ea
Jonathan Chen <jonathanbaby88@gmail.com>
2019-02-27 02:12:45 +0000

    Added AJAX

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+//var custom_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+var custom_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 
 $("#search-button").click(function(){
-  
-  
-  
+    $.ajax({
+        url: custom_url,
+        method: "GET",
+        success: function(response){
+            $(".row.gallery").replaceWith(`<img src = ${response.data[0].images.original.url} />`);
+        }
+    });
 });
 
