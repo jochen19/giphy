e861a5342b56a3184fbd4868dac39990c871cf29
Jonathan Chen <jonathanbaby88@gmail.com>
2019-03-06 02:04:14 +0000

    Added user input and random button

--- js/script.js
+++ js/script.js
@@ -3,14 +3,34 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 //var custom_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
-var custom_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+function random(max){
+    return Math.floor(Math.random(max)*Math.floor(max));
+}
 
-$("#search-button").click(function(){
+$("#all-button").click(function(){
+    var searchTerm = $("#search-term").val();
+    var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${searchTerm} &rating=pg&api_key=dc6zaTOxFJmzC`;
+    $.ajax({
+       url:custom_url,
+       mehtod: "GET",
+       success: function(response){
+           $(response.data).forEach(function()){
+               
+           }
+           $(".results").forEach.append(`<img src = ${response.data[""].images.original.url} />`);
+       }
+    });
+});
+
+$("#ran-button").click(function(){
+    var searchTerm = $("#search-term").val();
+    var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${searchTerm} &rating=pg&api_key=dc6zaTOxFJmzC`;
     $.ajax({
         url: custom_url,
         method: "GET",
         success: function(response){
-            $(".row.gallery").replaceWith(`<img src = ${response.data[0].images.original.url} />`);
+            var num = response.data.length
+            $(".results").append(`<img src = ${response.data[random(num)].images.original.url} />`);
         }
     });
 });
