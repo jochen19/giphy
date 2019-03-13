8b6ac5070b975dbcf1cc93b92701e1064dbb5255
Jonathan Chen <jonathanbaby88@gmail.com>
2019-03-13 00:54:00 +0000

    Added a random function and a search all function

--- js/script.js
+++ js/script.js
@@ -7,6 +7,35 @@ function random(max){
     return Math.floor(Math.random(max)*Math.floor(max));
 }
 
+$("button").click(function(){
+    $("#text").hide();
+});
+
+$("#ran-button").click(function(){
+    var custom_url = `https://api.giphy.com/v1/gifs/search?q=funny&rating=pg&api_key=dc6zaTOxFJmzC`;
+    $.ajax({
+       url:custom_url,
+       mehtod: "GET",
+       success: function(response){
+            var num = response.data.length;
+            $(".results").append(`<img src = ${response.data[random(num)].images.original.url} />`);
+        }    
+    });
+});
+
+$("#searchRan-button").click(function(){
+    var searchTerm = $("#search-term").val();
+    var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${searchTerm} &rating=pg&api_key=dc6zaTOxFJmzC`;
+    $.ajax({
+        url: custom_url,
+        method: "GET",
+        success: function(response){
+            var num = response.data.length;
+            $(".results").append(`<img src = ${response.data[random(num)].images.original.url} />`);
+        }
+    });
+});
+
 $("#all-button").click(function(){
     var searchTerm = $("#search-term").val();
     var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${searchTerm} &rating=pg&api_key=dc6zaTOxFJmzC`;
@@ -14,24 +43,9 @@ $("#all-button").click(function(){
        url:custom_url,
        mehtod: "GET",
        success: function(response){
-           $(response.data).forEach(function()){
-               
+           for (var i=0; i<response.data.length; i++){
+                $(".results").append(`<img src = ${response.data[i].images.original.url} />`);
            }
-           $(".results").forEach.append(`<img src = ${response.data[""].images.original.url} />`);
-       }
+        }    
     });
 });
-
-$("#ran-button").click(function(){
-    var searchTerm = $("#search-term").val();
-    var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${searchTerm} &rating=pg&api_key=dc6zaTOxFJmzC`;
-    $.ajax({
-        url: custom_url,
-        method: "GET",
-        success: function(response){
-            var num = response.data.length
-            $(".results").append(`<img src = ${response.data[random(num)].images.original.url} />`);
-        }
-    });
-});
-
